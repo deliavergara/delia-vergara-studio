b3a187ccb36966bd75b324f9e398e2450741e3f0
deliavergara <deliavergara.joyas@gmail.com>
2025-08-24 10:38:50 -0400

    new urls videos

--- src/pages/CategoryPage.tsx
+++ src/pages/CategoryPage.tsx
@@ -28,7 +28,7 @@ const CategoryPage = () => {
 
   // Mapeo de imágenes de portada para cada categoría
   const categoryPortraits = {
-         anillos: buildSupabaseUrl('productos/anillos/portada_anillos/anillo_portada.jpg'),
+         anillos: buildSupabaseUrl('productos/anillos/portada_anillos/portada_anillos_4.jpg'),
          collares: buildSupabaseUrl('productos/collares/portada_collares/portada_collares_3.jpg'),
          pendientes: buildSupabaseUrl('productos/pendientes/portada_pendientes/portada_pendientes_4.jpg'),
          pulseras: buildSupabaseUrl('productos/pulseras/portada_pulseras/portada_pulsera_1.jpg')
